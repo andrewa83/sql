98e3af2dd03ac3e9a1d3974f25b59a88f92ad0de
unknown <anayotte@gmail.com>
2022-07-05 20:24:23 -0400

    Created table for books

---
+++ createtable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Pirates (
+    BookID int,
+    Title varchar(255),
+    AuthorLastName varchar(255),
+    YearPublished int,
+    Pages int
+);
+
+CREATE TABLE Trains (
+    BookID int,
+    Title varchar(255),
+    AuthorLastName varchar(255),
+    YearPublished int,
+    Pages int
+);
