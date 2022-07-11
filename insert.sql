1039f5f51ab8b7a99f4ad98afd615a87a5e9cb10
unknown <anayotte@gmail.com>
2022-07-11 13:43:56 -0400

    Insert values into table

---
+++ insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO Pirates
+VALUES (1, 'Pirates: Predators of the Seas', 'Konstam & Kean',
+       2006, 248);
+
+INSERT INTO Trains
+VALUES (1, '100 Trains 100 Years: A Century of Locomotives and Trains', 'Winkowski & Sullivan',
+       2000, 172);
+
